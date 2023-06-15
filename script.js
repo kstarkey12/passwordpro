afde5578d10880231984a6d2ae73f2dd6e2f6aab
konnor starkey <130928028+kstarkey12@users.noreply.github.com>
2023-06-14 23:07:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  const loginForm = document.getElementById('login-form');
+  const noteContainer = document.getElementById('note-container');
+  const noteInput = document.getElementById('note-input');
+  const saveButton = document.getElementById('save-button');
+  const logoutButton = document.getElementById('logout-button');
+
+  const savedNote = localStorage.getItem('note');
+  if (savedNote) {
+    noteInput.value = savedNote;
+  }
+
+  function authenticate(password) {
+    // Implement server-side authentication logic here
+    // Compare the provided password with the stored password hash
+    // Return true if authenticated, false otherwise
+  }
+
+  function saveNote() {
+    const note = noteInput.value;
+    localStorage.setItem('note', note);
+    alert('Note saved successfully!');
+  }
+
+  function logout() {
+    localStorage.removeItem('note');
+    noteInput.value = '';
+    loginForm.classList.remove('hidden');
+    noteContainer.classList.add('hidden');
+  }
+
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const password = document.getElementById('password').value;
+    if (authenticate(password)) {
+      loginForm.classList.add('hidden');
+      noteContainer.classList.remove('hidden');
+    } else {
+      alert('Invalid password. Please try again.');
+    }
+  });
+
+  saveButton.addEventListener('click', saveNote);
+
+  logoutButton.addEventListener('click', logout);
+});
 
